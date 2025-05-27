26a7d2e38ccebe309fb2d0745f3d21cf1bf16c7e
Paolagbr <150736512+Paolagbr@users.noreply.github.com>
2025-05-27 17:34:04 -0600

    FirestoreConfigurada

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,19 @@ import { HttpClientModule } from '@angular/common/http';
 import { importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 bootstrapApplication(AppComponent, {
   providers: [
     importProvidersFrom(HttpClientModule),
-    provideRouter(routes)
+    provideRouter(routes), provideFirebaseApp(() => initializeApp({ 
+      projectId: "proyectofinaltw-73dc0",
+       appId: "1:691281619346:web:7779b6975eebab2b8cb94b", 
+       storageBucket: "proyectofinaltw-73dc0.firebasestorage.app", 
+       apiKey: "AIzaSyBJXHh0cp0E3_9rJOWV1i54gr9DTGmsSw0",
+        authDomain: "proyectofinaltw-73dc0.firebaseapp.com", 
+        messagingSenderId: "691281619346" })), provideFirestore(() => getFirestore())
   ]
 })
   .catch((err) => console.error(err));
