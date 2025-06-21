b5c9d072a50c15cfc470f2ad411f278437c21173
Eduardo2106 <eduardogg288@gmail.com>
2025-06-20 23:53:47 -0600

    union de todos los avances

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,18 +1,7 @@
-import { ApplicationConfig, provideZoneChangeDetection, importProvidersFrom } from '@angular/core';
-import { provideRouter } from '@angular/router';
-import { provideHttpClient } from '@angular/common/http';
-import { provideAnimations } from '@angular/platform-browser/animations';
-
-import { FormsModule } from '@angular/forms';
-import { CommonModule } from '@angular/common';
-import { routes } from './app.routes';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }), 
-    provideRouter(routes),
-    provideHttpClient(),
-    provideAnimations(),
-    importProvidersFrom(FormsModule, CommonModule)
+    provideZoneChangeDetection({ eventCoalescing: true })
   ]
 };
--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,39 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-import { HttpClientModule } from '@angular/common/http';
-import { importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { routes } from './app/app.routes';
+import { provideHttpClient } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { importProvidersFrom } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common'; 
+
+// Firebase imports
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
-import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+
+import { routes } from './app/app.routes';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBJXHh0cp0E3_9rJOWV1i54gr9DTGmsSw0",
+  authDomain: "proyectofinaltw-73dc0.firebaseapp.com",
+  projectId: "proyectofinaltw-73dc0",
+  storageBucket: "proyectofinaltw-73dc0.firebasestorage.app",
+  messagingSenderId: "691281619346",
+  appId: "1:691281619346:web:7779b6975eebab2b8cb94b",
+  measurementId: "G-4HJ1KPNT7K"
+};
 
 bootstrapApplication(AppComponent, {
   providers: [
-    importProvidersFrom(HttpClientModule),
-    provideRouter(routes), provideFirebaseApp(() => initializeApp({ 
-      projectId: "proyectofinaltw-73dc0",
-       appId: "1:691281619346:web:7779b6975eebab2b8cb94b", 
-       storageBucket: "proyectofinaltw-73dc0.firebasestorage.app", 
-       apiKey: "AIzaSyBJXHh0cp0E3_9rJOWV1i54gr9DTGmsSw0",
-        authDomain: "proyectofinaltw-73dc0.firebaseapp.com", 
-        messagingSenderId: "691281619346" })), provideFirestore(() => getFirestore()), provideFirebaseApp(() => initializeApp({ projectId: "proyectofinaltw-73dc0", appId: "1:691281619346:web:7779b6975eebab2b8cb94b", storageBucket: "proyectofinaltw-73dc0.firebasestorage.app", apiKey: "AIzaSyBJXHh0cp0E3_9rJOWV1i54gr9DTGmsSw0", authDomain: "proyectofinaltw-73dc0.firebaseapp.com", messagingSenderId: "691281619346" })), provideFirestore(() => getFirestore())
+    provideRouter(routes),
+    provideHttpClient(),
+    provideAnimations(),
+    importProvidersFrom(FormsModule, CommonModule), 
+    
+    // Firebase providers
+    provideFirebaseApp(() => initializeApp(firebaseConfig)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
   ]
-})
-  .catch((err) => console.error(err));
+}).catch(err => console.error(err));
