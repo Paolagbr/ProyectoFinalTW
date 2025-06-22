a41e3e7bd4bf8cd8b142abdf7b21c53ffc7260e2
Eduardo2106 <eduardogg288@gmail.com>
2025-06-21 23:32:00 -0600

    IMPORTANTE, CON ESTE COMMIT LA RAMA EDUARDO QUEDA ACTUALIZADA 100% CON TODO LO QUE SE LLEVA DEL PROYECTO

--- src/app/cita/cita.component.ts
+++ src/app/cita/cita.component.ts
@@ -1,23 +1,65 @@
-import { Component } from "@angular/core"
-import { HttpClientModule } from "@angular/common/http"
-import { CommonModule } from "@angular/common"
-import { FormsModule } from "@angular/forms"
+import { Component, ChangeDetectionStrategy, OnInit } from "@angular/core";
+import { HttpClientModule, HttpClient } from "@angular/common/http";
+import { CommonModule } from "@angular/common";
+import { FormsModule } from "@angular/forms";
+
+
+import { hora, lista, sexo, userInfo } from '../datos';
+
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatDatepicker, MatDatepickerInputEvent, MatDatepickerModule } from '@angular/material/datepicker';
+import { MatOptionModule, provideNativeDateAdapter } from '@angular/material/core';
+import { MatRadioModule } from '@angular/material/radio';
+import Swal from 'sweetalert2';
+import { MatTimepickerInput, MatTimepickerModule } from '@angular/material/timepicker';
+import { MatSelectModule } from '@angular/material/select';
+import { HORA } from '../grupo';
+import { MatCardModule } from '@angular/material/card';
+import { UsuariosService } from '../servicios/usuarios.service';
+import { Auth } from '@angular/fire/auth';
+import { RegistrarCitaUsuarioComponent } from '../registrar-cita-usuario/registrar-cita-usuario.component';
+import { JsonPipe } from '@angular/common';
+
+export interface Place {
+  name: string;
+  grupo: number;
+  sexo: string;
+  fechaCita: string;
+  hora: string;
+}
 
 @Component({
-  selector: "app-servicios-spa",
+  selector: "app-formulario",
   standalone: true,
-  imports: [HttpClientModule, CommonModule, FormsModule],
-  templateUrl: "./servicios-spa.component.html",
-  styleUrls: ["./servicios-spa.component.css"],
+  imports: [
+    FormsModule,
+    JsonPipe,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatRadioModule,
+    MatTimepickerModule,
+    MatSelectModule,
+    MatInputModule,
+    MatOptionModule,
+    CommonModule,
+    MatCardModule,
+    RegistrarCitaUsuarioComponent,
+    HttpClientModule
+  ],
+  providers: [provideNativeDateAdapter()],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  templateUrl: './cita.component.html',
+  styleUrls: ['./cita.component.css']
 })
-export class CitaComponent {
+export class CitaComponent implements OnInit {
   mostrarFormulario = false;
   usuarioAutenticado = false;
 
   constructor(private auth: Auth, private http: HttpClient) {}
+
   private apiUrl = 'http://localhost:3000/enviar-cita'; //Conectar con node, para que permita el envio de información 
-
-
 
   enviarCita(cita: any) {
     return this.http.post(this.apiUrl, cita);
@@ -33,9 +75,7 @@ export class CitaComponent {
     if (this.usuarioAutenticado) {
       this.mostrarFormulario = true;
     } else {
-       Swal.fire('Se requiere iniciar sesión');
+      Swal.fire('Se requiere iniciar sesión');
     }
   }
 }
-
-// Aquí se define el componente CitaComponent, que maneja la lógica del formulario de citas
